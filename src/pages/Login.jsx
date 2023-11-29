38030dd2ac04647584e194b2b68b25050b31d3e1
GolAndres <ag1480885@gmail.com>
2023-11-28 20:57:42 -0500

    28 de noviembre

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,10 +8,10 @@ const Login = () => {
   console.log(state)
   return (
     <div style={{height: '100vh', width: '100%', backgroundImage: "url('/img/LoginSteam.png')", backgroundPosition: 'center', backgroundSize: 'cover'}}>
-      
+
       <form action="">
       <input type="text" placeholder='email' name="email" onChange={({target:{value, name}}) => OnchangeForm(value, name )}/>
-      <input type="text" placeholder='password' name="password" onChange={(e) => console.log(e)}/>
+      <input type="password" placeholder='password' name="password" onChange={({target:{value, name}}) => OnchangeForm(value, name )}/>
       </form>
     </div>
   )
