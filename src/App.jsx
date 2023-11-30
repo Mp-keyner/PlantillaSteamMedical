e83b93662eff157855e249d98551fdfa7373fc8f
ana03ap <anxrdilx03@gmail.com>
2023-11-30 08:26:59 -0500

    agregando

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Layout from './components/Layout/'
+import Layout from './components/Layout'
 import Login from './pages/Login'
 
 const App = () => {
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import MainPages from '../pages/MainPages'
 
 const Layout = () => {
   return (
     <>
         <Routes>
-        <Route path='/Login' element={<Login/>} />
+        <Route path='/' element={<MainPages/>} />
         </Routes>
     </>
   )
